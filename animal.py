3de56a759733b28d11ac08644e323992a5c192bb
arrisons <arrisons@oregonstate.edu>
2026-01-07 15:11:07 -0800

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Sylvan Arrison
+# GitHub username: arrisons
+# Date: 1/7/2026
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal:")
+print("Your favorite animal is the " + fave_animal + ".")
